e2e980eaa4e028361c9898b21af4c0b29c87b8c6
ChristopherBraun196 <232182424+ChristopherBraun196@users.noreply.github.com>
2026-05-25 18:23:42 +0200

    start with angular modul one

--- product-list/src/app/app.ts
+++ product-list/src/app/app.ts
@@ -2,13 +2,41 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
 import { ProductList } from './shared/components/product-list/product-list';
+import { ProductDetail } from './shared/components/product-detail/product-detail';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, ProductList],
+  imports: [RouterOutlet, Header, ProductList, ProductDetail],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
-  protected readonly title = signal('product-list');
+  lang = "de"
+
+  errormsg = ""
+   
+  list = []
 }
+
+// export class App {
+  // protected readonly title = signal('product-list');
+  // our_text = "banana love"
+
+  // ngOnInit(){
+  //   this.our_text = "das ist ein hook text"
+  // }
+
+  // textGen (){
+  //   this.our_text = "banana love 2.0"
+
+  // }
+
+  // isDisabled = false
+
+  // getBtnColor(){
+  //   return 'blue'
+  // }
+// }
---
+++ product-list/src/app/shared/components/product-detail/product-detail.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  imports: [],
+  templateUrl: './product-detail.html',
+  styleUrl: './product-detail.scss',
+})
+
+
+export class ProductDetail {
+  detail = {
+    name: 'Gaming Maus',
+    description:
+      'Eine ergonomische Gaming-Maus mit hoher Präzision und einstellbarer DPI. Ideal für FPS- und MOBA-Spiele, bietet sie eine langlebige Bauweise und komfortableSeitentasten für schnelles Reagieren.',
+    specs: 'dpi: 6400, cable length: 1.8m, color: Schwarz',
+    stock: 120,
+    price: 29.99,
+  };
+
+  deleteDetail(){
+    this.detail.name = ""
+  }
+}
